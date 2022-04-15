d1ad5b5c03049f425a290c895a6bcc98e955e963
svnday <40030806+svnday@users.noreply.github.com>
2022-04-15 15:00:21 -0500

    added additional text to high score list elements

--- assets/highscore.js
+++ assets/highscore.js
@@ -15,7 +15,7 @@ if (allScores !== null) {
     for (var i = 0; i < allScores.length; i++) {
 
         var createLi = document.createElement("li");
-        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        createLi.textContent = allScores[i].initials + "'s high score:" + allScores[i].score;
         highScore.appendChild(createLi);
 
     }
