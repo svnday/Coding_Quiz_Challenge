2713cc759cfce7ce4515781e4a48dbdaba9e7164
svnday <40030806+svnday@users.noreply.github.com>
2022-04-15 14:52:21 -0500

    wrote the front end and styled it, then went ahead and wrote all of the javascript as well

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# coding_quiz_challenge
+# Coding Quiz Challenge
+This app is for Module 4: Web APIs  in my full stack web developer bootcamp.
+
+This is a website that provides the user with a quiz of a few code-themed questions. While the quiz is running, there is a dynamic timer in the top corner that also serves as the user's score. When a question is incorrect, time is deducted. Once all of the questions have been answered, the remaining time is the user's score.
+
+Scores are then saved and can be viewed in the high scores page.
+
+## User Story
+
+```
+AS A coding boot camp student  
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores  
+SO THAT I can gauge my progress compared to my peers  
+
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I am taking a code quiz  
+WHEN I click the start button  
+THEN a timer starts and I am presented with a question  
+WHEN I answer a question  
+THEN I am presented with another question  
+WHEN I answer a question incorrectly  
+THEN time is subtracted from the clock  
+WHEN all questions are answered or the timer reaches 0  
+THEN the game is over  
+WHEN the game is over  
+THEN I can save my initials and score  
+```
+
+## Technology Used
+```
+This project uses the following technologies:  
+HTML
+CSS
+Javascript
+```
+
+## Instructions For Use
+```
+This application can be used by navigating to [https://searching-for-books-svnday.herokuapp.com/](https://searching-for-books-svnday.herokuapp.com/)
+
+If running on your local machine, clone the repository.  
+While in the root directory, use the "Open in default browser" option when clicking on index.html.
+```
+
+____________________________________________________________________________________________________
+
+
+Link to repository: [https://github.com/svnday/searching-for-books](https://github.com/svnday/searching-for-books)  
+Link to deployed application: [https://searching-for-books-svnday.herokuapp.com/](https://searching-for-books-svnday.herokuapp.com/)  
+Link to screenshot: ![https://i.imgur.com/ylRrXmk.png](https://i.imgur.com/ylRrXmk.png)  
---
+++ assets/script.js
@@ -0,0 +1,186 @@
+var questions = [
+    {
+        title: "Which symbol is used to reference a CLASS in CSS?",
+        choices: [".", "#", "$", "%"],
+        answer: "."
+    },
+    {
+        title: "What is the way to log something to your browser's console?",
+        choices: ["console-log()", "consoleLog()", "console.log()", "console&log()"],
+        answer: "console.log()"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "What is the maximum number of objects that can be in an array?",
+        choices: ["100", "15", "10", "An array can have an infinite amount of index items."],
+        answer: "An array can have an infinite amount of index items."
+    },
+    {
+        title: "What does CSS stand for?",
+        choices: ["Clean Style Script", "Code Super Style", "Code Style Sheet", "Cascading Style Sheet"],
+        answer: "Cascading Style Sheet"
+    },
+
+];
+
+var score = 0;
+var questionIndex = 0;
+
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTime");
+var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#wrapper");
+
+var secondsLeft = 76;
+
+var holdInterval = 0;
+
+var penalty = 10;
+
+var ulCreate = document.createElement("ul");
+
+timer.addEventListener("click", function () {
+
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            secondsLeft--;
+            currentTime.textContent = "Time: " + secondsLeft;
+
+            if (secondsLeft <= 0) {
+                clearInterval(holdInterval);
+                allDone();
+                currentTime.textContent = "Time's up!";
+            }
+        }, 1000);
+    }
+    render(questionIndex);
+});
+
+function render(questionIndex) {
+    questionsDiv.innerHTML = "";
+    ulCreate.innerHTML = "";
+
+    for (var i = 0; i < questions.length; i++) {
+        var userQuestion = questions[questionIndex].title;
+        var userChoices = questions[questionIndex].choices;
+        questionsDiv.textContent = userQuestion;
+    }
+    userChoices.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        questionsDiv.appendChild(ulCreate);
+        ulCreate.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
+}
+
+function compare(event) {
+    var element = event.target;
+
+    if (element.matches("li")) {
+        var createDiv = document.createElement("div");
+        createDiv.setAttribute("id", "createDiv");
+
+        if (element.textContent == questions[questionIndex].answer) {
+            score++;
+            createDiv.textContent = "Correct! The answer is:  " + questions[questionIndex].answer;
+            
+        } else {
+            secondsLeft = secondsLeft - penalty;
+            createDiv.textContent = "Wrong! The correct answer is:  " + questions[questionIndex].answer;
+        }
+
+    }
+    questionIndex++;
+
+    if (questionIndex >= questions.length) {
+        allDone();
+        createDiv.textContent = "End of the quiz!" + " " + "You got  " + score + "/" + questions.length + " correct!";
+    } else {
+        render(questionIndex);
+    }
+    questionsDiv.appendChild(createDiv);
+
+}
+function allDone() {
+    questionsDiv.innerHTML = "";
+    currentTime.innerHTML = "";
+
+    var createH1 = document.createElement("h1");
+    createH1.setAttribute("id", "createH1");
+    createH1.textContent = "All done!"
+
+    questionsDiv.appendChild(createH1);
+
+    var createP = document.createElement("p");
+    createP.setAttribute("id", "createP");
+
+    questionsDiv.appendChild(createP);
+
+    if (secondsLeft >= 0) {
+        var timeRemaining = secondsLeft;
+        var createP2 = document.createElement("p");
+        clearInterval(holdInterval);
+        createP.textContent = "Your final score is: " + timeRemaining;
+
+        questionsDiv.appendChild(createP2);
+    }
+
+    // label for saving initials/high score
+    var createLabel = document.createElement("label");
+    createLabel.setAttribute("id", "createLabel");
+    createLabel.textContent = "Enter your initials: ";
+
+    questionsDiv.appendChild(createLabel);
+
+    // inputs
+    var createInput = document.createElement("input");
+    createInput.setAttribute("type", "text");
+    createInput.setAttribute("id", "initials");
+    createInput.textContent = "";
+
+    questionsDiv.appendChild(createInput);
+
+    // submit info
+    var createSubmit = document.createElement("button");
+    createSubmit.setAttribute("type", "submit");
+    createSubmit.setAttribute("id", "Submit");
+    createSubmit.textContent = "Submit";
+
+    questionsDiv.appendChild(createSubmit);
+
+    // save info and score to localstorage
+    createSubmit.addEventListener("click", function () {
+        var initials = createInput.value;
+
+        if (initials === null) {
+
+            console.log("No value entered!");
+
+        } else {
+            var finalScore = {
+                initials: initials,
+                score: timeRemaining
+            }
+            console.log(finalScore);
+            var allScores = localStorage.getItem("allScores");
+            if (allScores === null) {
+                allScores = [];
+            } else {
+                allScores = JSON.parse(allScores);
+            }
+            allScores.push(finalScore);
+            var newScore = JSON.stringify(allScores);
+            localStorage.setItem("allScores", newScore);
+
+            // high score page
+            window.location.replace("./highscores.html");
+        }
+    });
+    
+
+}
